ed7166ec77e8e2a12345f0448717262a054901c3
mattsteencpp <mattsteen@gmail.com>
2019-06-29 14:31:17 -0400

    Add ability to set block size in minutes

--- main/main.go
+++ main/main.go
@@ -14,9 +14,10 @@ import (
 const timeFormat = "3:04pm"
 
 type TrackerData struct {
-	Time    string         `yaml:"time"`
-	Entries []TrackerEntry `yaml:"entries"`
-	Todos   []TrackerTodo  `yaml:"todos"`
+	Time      string         `yaml:"time"`
+	BlockSize int64          `yaml:"block_size"`
+	Entries   []TrackerEntry `yaml:"entries"`
+	Todos     []TrackerTodo  `yaml:"todos"`
 }
 
 type TrackerEntry struct {
@@ -54,7 +55,7 @@ func updateValue(data *TrackerData, action, input string) {
 		fmt.Printf("Failed to parse multiplier: %v\n", err)
 		os.Exit(1)
 	}
-	delta *= 0.25
+	delta *= float64(data.BlockSize)
 	if action == "s" || action == "subtract" {
 		delta *= -1
 	}
@@ -66,7 +67,8 @@ func updateValue(data *TrackerData, action, input string) {
 	if data.Entries[idx].Total < 0 {
 		delta -= data.Entries[idx].Total
 		data.Entries[idx].Total = 0.0
-		fmt.Printf("Negative totals are not permitted. Setting to 0...\n\n")
+		fmt.Printf("Negative totals are not permitted\n\n")
+		os.Exit(1)
 	}
 
 	updateTime(data, delta)
@@ -82,8 +84,27 @@ func getLogTime(data *TrackerData) time.Time {
 	return logTime
 }
 
+func setBlockSize(data *TrackerData, size string) {
+	blockSize, err := strconv.ParseInt(size, 10, 64)
+	if err != nil {
+		fmt.Printf("Failed to parse block size (must be an integer): %v\n", err)
+		os.Exit(1)
+	}
+	if blockSize < 1 || blockSize > 60 {
+		fmt.Printf("Block size must be between 1 and 60 minutes: %v\n", err)
+		os.Exit(1)
+	}
+	for i := 0; i < len(data.Entries); i++ {
+		if data.Entries[i].Total > 0 {
+			fmt.Printf("Block size cannot be updated after logging time for the day (total must be 0)\n")
+			os.Exit(1)
+		}
+	}
+	data.BlockSize = blockSize
+}
+
 func smartUpdateTime(data *TrackerData) {
-	currentDateTime := time.Now().Round(15 * time.Minute)
+	currentDateTime := time.Now().Round(time.Duration(data.BlockSize) * time.Minute)
 	data.Time = currentDateTime.Format(timeFormat)
 }
 
@@ -100,8 +121,8 @@ func smartUpdateValue(data *TrackerData, letter string) {
 		os.Exit(1)
 	}
 
-	timeBlocks := currentTime.Sub(logTime).Round(15 * time.Minute)
-	delta := timeBlocks.Minutes() / 60.0
+	timeBlocks := currentTime.Sub(logTime).Round(time.Duration(data.BlockSize) * time.Minute)
+	delta := timeBlocks.Minutes()
 
 	idx := letterToIdx(letter)
 	data.Entries[idx].Total += delta
@@ -111,7 +132,7 @@ func smartUpdateValue(data *TrackerData, letter string) {
 
 func updateTime(data *TrackerData, delta float64) {
 	logTime := getLogTime(data)
-	logTime = logTime.Add(time.Minute * time.Duration(int(60*delta)))
+	logTime = logTime.Add(time.Minute * time.Duration(delta))
 	newTime := logTime.Format(timeFormat)
 	data.Time = newTime
 }
@@ -134,6 +155,17 @@ func resetEntries(data *TrackerData) {
 	updateTime(data, totalTime)
 }
 
+func formatDuration(totalMinutes float64) string {
+	if totalMinutes == 0.0 {
+		return "0"
+	}
+	duration := time.Duration(totalMinutes) * time.Minute
+	hours := duration / time.Hour
+	duration -= hours * time.Hour
+	minutes := duration / time.Minute
+	return fmt.Sprintf("%d:%02d", hours, minutes)
+}
+
 func printState(data TrackerData) {
 	entries := data.Entries
 	totalTime := 0.0
@@ -143,9 +175,11 @@ func printState(data TrackerData) {
 		if entry.Name != "lunch" && entry.Name != "me time" {
 			totalTime += entry.Total
 		}
-		fmt.Printf("%s) %s: %vh\n", letter, entry.Name, entry.Total)
+
+		formattedDuration := formatDuration(entry.Total)
+		fmt.Printf("%s) %s: %vh\n", letter, entry.Name, formattedDuration)
 	}
-	fmt.Printf("Total: %vh\n", totalTime)
+	fmt.Printf("Total: %vh\n", formatDuration(totalTime))
 	logTime := data.Time
 	fmt.Printf(logTime)
 	fmt.Printf("\n\n")
@@ -201,9 +235,10 @@ func printHelp() {
 	fmt.Println("v1.0.0")
 	fmt.Println("Usage: ")
 	fmt.Println("'dt' to get current status")
-	fmt.Println("'dt add a' to add 15 minutes to entry a")
-	fmt.Println("'dt add 3b' to add 45 minutes to entry b")
-	fmt.Println("'dt subtract 2b' to subtract 30 minutes from entry b")
+	fmt.Println("'dt add a' to add a block to entry a")
+	fmt.Println("'dt add 3b' to add 3 blocks to entry b")
+	fmt.Println("'dt subtract 2b' to subtract 2 blocks from entry b")
+	fmt.Println("'dt blocksize 10' to set the block size going forward to 10 minutes")
 	fmt.Println("'dt time 8:00am' to set today's start time to 8am")
 	fmt.Println("'dt start' to set today's start time to the current time")
 	fmt.Println("'dt new \"project 1\" to add a new entry called 'project 1'")
@@ -240,6 +275,9 @@ func main() {
 	} else if action == "t" || action == "time" { // set start time for the day
 		newTime := flag.Arg(1)
 		setTime(&trackerData, newTime)
+	} else if action == "b" || action == "blocksize" { // set block size for future updates
+		size := flag.Arg(1)
+		setBlockSize(&trackerData, size)
 	} else if action == "st" || action == "start" { // set start time to the current time
 		smartUpdateTime(&trackerData)
 	} else if action == "r" || action == "reset" { // reset all entries
